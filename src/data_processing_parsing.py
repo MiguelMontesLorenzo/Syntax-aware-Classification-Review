545b9d3fed7203056cd85113f3e2da085f0ec663
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2024-03-31 11:55:46 +0200

    Created functions to download parsing data. We created the functions to download the treebank data from the nlkt library and save it ina given path. Later on we extracted the grammar rules from the treebank for the parsing in a .txt file.

---
+++ src/data_processing_parsing.py
@@ -0,0 +1,90 @@
+import nltk
+from nltk.corpus.reader.bracket_parse import BracketParseCorpusReader
+
+import os
+from typing import List, Tuple
+
+
+class Treebank():
+    def __init__(self):
+        self._rules: dict = {}
+
+
+def extract_rules(tree: nltk.Tree, rules: List) -> List[Tuple[str, str]]:
+    '''
+    Recursively extract rules from an nltk.Tree object.
+
+    Args:
+    - tree: nltk.Tree
+        The tree from which rules are to be extracted.
+    - rules: list
+        A list to store the extracted rules. Each rule is represented 
+        as a tuple (label, child_labels).
+
+    Returns:
+    - rules: list
+        List of extracted rules.
+    '''
+
+    if isinstance(tree, nltk.Tree):
+        rules.append((tree.label(), ' '.join(child.label() if isinstance(child, nltk.Tree) else child for child in tree)))
+        for child in tree:
+            extract_rules(child, rules)
+    return rules
+
+
+def load_data() -> None:
+    """
+    Load data from PRD files, extract grammar rules, and write them to a
+    text file.
+
+    This function downloads data from a specified path, reads PRD files,
+    extracts grammar rules from parsed sentences, and writes the rules
+    to a text file named 'grammar_rules.txt' in the same directory.
+
+    Args:
+    - None
+
+    Returns:
+    - None
+    """
+    # Path to the directory containing your PRD files
+    path: str = "data_parsing"
+
+    # Download data
+    download_data(path)
+
+    # Initialize a BracketParseCorpusReader
+    prd_reader: BracketParseCorpusReader = BracketParseCorpusReader(path, r".*\.prd")
+
+    # Get a list of file identifiers (fileids)
+    fileids: List[str] = prd_reader.fileids()
+
+    # Read the trees from the iles
+    with open(path + "/grammar_rules.txt", 'w') as file:
+        for fileid in fileids:
+            trees: List[nltk.Tree] = prd_reader.parsed_sents(fileid)
+            for tree in trees:
+                rules: List[Tuple[str, str]] = []
+                rules = extract_rules(tree, rules)
+                for rule in rules:
+                    file.write(rule[0] + ' -> ' + rule[1] + '\n')
+                file.write('\n')
+
+
+def download_data(path: str) -> None:
+    '''
+    Download the data from nltk and save it  in the given directory
+
+    Args:
+    - path: string with the path where the files will be downloaded
+
+    Returns:
+    - None
+    '''
+    # Checking if provided directory exist and if not create it
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+        # Download the Penn Treebank corpus
+        nltk.download('treebank', download_dir=path)
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.data_processing_parsing import load_data
+
+
+def main():
+    load_data()
+
+
+if __name__ == "__main__":
+    main()
