98bb8acaa93d17c1e641219cd723bf231e3eed58
Manuel Rodriguez <manuel.rodriguezvillegas09@gmail.com>
2024-04-14 11:17:30 +0200

    New embeddings model created

---
+++ src_v2/main.py
@@ -0,0 +1,73 @@
+import torch
+from torch.utils.data import DataLoader
+import os
+try:
+    from data_processing import load_and_preprocess_data, subsample_words, generate_data_loader
+    from skipgram import SkipGramNeg
+    from train import train_skipgram
+    from utils import plot_embeddings, save_model
+except:
+    from src_v2.data_processing import load_and_preprocess_data, subsample_words, generate_data_loader
+    from src_v2.skipgram import SkipGramNeg
+    from src_v2.train import train_skipgram
+    from src_v2.utils import plot_embeddings, save_model
+
+def main():
+    print("Starting the SkipGram training pipeline...")
+
+    embedding_dim: int = 300
+    batch_size: int = 512
+    epochs: int = 5
+    learning_rate: float = 0.003
+    print_every: int = 1500
+    runs_folder: str = "runs"
+    model_filename: str = "skipgram_dep_model_small.pth"
+    model_path: str = os.path.join(runs_folder, model_filename)
+    train_model: bool = True
+    device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    print("Step 1: Loading and preprocessing data...")
+    sentences, tokens, correspondences, vocab_to_int, int_to_vocab = load_and_preprocess_data()
+    print(f"Loaded and preprocessed {len(tokens)} tokens.")
+    print(f"Created vocabulary with {len(vocab_to_int)} unique words.")
+
+    if train_model:
+        print("Step 3: Subsampling frequent words...")
+        train_words, freqs, sampled_correspondences = subsample_words(tokens, vocab_to_int, correspondences)
+        print(f"Subsampled words to {len(train_words)} training examples.")
+
+        print("Step 4: Creating DataLoader...")
+        dataloader: DataLoader = generate_data_loader(train_words, sentences, sampled_correspondences, batch_size, vocab_to_int, int_to_vocab)
+
+        # Calculate the noise distribution for negative sampling
+        print("Calculating noise distribution for negative sampling...")
+        word_freqs = torch.tensor(sorted(freqs.values(), reverse=True))
+        unigram_dist = word_freqs / word_freqs.sum()
+        noise_dist = (unigram_dist ** 0.75 / torch.sum(unigram_dist ** 0.75)).detach().clone().to(device)
+
+        print("Step 5: Initializing the SkipGram model...")
+        model = SkipGramNeg(len(vocab_to_int), embedding_dim, noise_dist=noise_dist).to(device)
+        print("Model initialized.")
+
+        print("Step 6: Training the model...")
+        train_skipgram(model, dataloader, int_to_vocab, epochs, learning_rate, print_every, device)
+        print("Training completed.")
+
+        print("Step 7: Saving the model...")
+        save_model(model, model_path)
+        print(f"Model saved at {model_path}")
+
+        print("Step 8: Visualizing the word embeddings...")
+    else:
+        print("Step 3: Loading train model...")
+        model: SkipGramNeg = SkipGramNeg(len(vocab_to_int), embedding_dim).to(device)
+        model.load_state_dict(torch.load(model_path))
+        model.eval()
+        print('Model Loaded.')
+        print("Step 4: Visualizing the word embeddings...")
+
+    plot_embeddings(model, int_to_vocab, viz_words=400)
+    print("Visualization complete.")
+
+if __name__ == "__main__":
+    main()
---
+++ src_v2/utils.py
@@ -0,0 +1,114 @@
+from typing import List, Dict, Tuple
+from collections import Counter
+import matplotlib.pyplot as plt
+import torch
+from sklearn.manifold import TSNE
+import os
+
+
+def clean_text(text: str) -> str:
+    """
+    Replaces symbols with characters that the model can use.
+
+    Args:
+    - text (str): text to be processed.
+
+    Returns:
+    - text (str): proecessed text.
+    """
+    substitutions: Dict[str, str] = {
+        '.': ' <PERIOD> ',
+        ',': ' <COMMA> ',
+        '"': ' <QUOTATION_MARK> ',
+        ';': ' <SEMICOLON> ',
+        '!': ' <EXCLAMATION_MARK> ',
+        '?': ' <QUESTION_MARK> ',
+        '(': ' <LEFT_PAREN> ',
+        ')': ' <RIGHT_PAREN> ',
+        '--': ' <HYPHENS> ',
+        '?': ' <QUESTION_MARK> ',
+        ':': ' <COLON> '
+    }
+    text = text.lower()
+    for key, value in substitutions.items():
+        text.replace(key, value)
+    return text
+
+
+def tokenize(sentences: List[str]) -> Tuple[List[str], Dict[int, str]]:
+    """
+    Processed the input sentences to extrcat the list of words and tokenize the text.
+
+    Args:
+    - sentences (List[str]): list of correctly processed sentences fomr both files.
+
+    Returns:
+    - trimmed_words (List[str]): list of trimmed (filtered) words.
+    - correspondences (Dict[int, Tuple(int)]): association of word index in tokens and the index of the sentence that
+    word belongs to.
+    """
+    correspondences: Dict[int, str] = {}
+
+    trimmed_words: List[str] = []
+    index: int = 0
+    for i in range(len(sentences)):
+        splitted_sentence: List[str] = sentences[i].split()
+        for j in range(len(splitted_sentence)):
+            word: str = splitted_sentence[j].lower()
+            trimmed_words.append(word)
+            correspondences[index] = (i, j)
+            index += 1
+
+    return trimmed_words, correspondences
+
+
+def plot_embeddings(model, int_to_vocab: Dict[int, str], viz_words=400, figsize=(16, 16)) -> None:
+    """
+    Plots a subset of word embeddings in a 2D space using t-SNE.
+
+    Args:
+    - model (SkipGram): trained SkipGram model containing the embeddings.
+    - int_to_vocab (Dict[int, str]): Dictionary mapping word indices back to words.
+    - viz_words (int): Number of words to visualize.
+    - figsize (Tuple[int]): Size of the figure for the plot.
+
+    Returns:
+    - None
+    """
+    # Extract embeddings
+    embeddings = model.in_embed.weight.to("cpu").data.numpy()
+    
+    # Reduce the dimensionality of embeddings with t-SNE
+    tsne = TSNE()
+    embed_tsne = tsne.fit_transform(embeddings[:viz_words, :])
+    
+    # Plotting
+    fig, ax = plt.subplots(figsize=figsize)
+    for idx in range(viz_words):
+        plt.scatter(*embed_tsne[idx, :], color="steelblue")
+        plt.annotate(int_to_vocab[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha=0.7)
+    
+    plt.show()
+
+
+def save_model(model, model_path="skipgram_dep_model.pth"):
+    """
+    Save the trained SkipGram model to a file, creating the directory if it does not exist.
+
+    Args:
+    - model (SkipGram): trained SkipGram model.
+    - model_path (str): path to save the model file, including directory and filename.
+
+    Returns:
+    - model_path (str): path where the model was saved.
+    """
+    # Extract the directory path from the model_path
+    directory = os.path.dirname(model_path)
+    
+    # Check if the directory exists, and create it if it does not
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+    
+    # Save the model
+    torch.save(model.state_dict(), model_path)
+    return model_path
