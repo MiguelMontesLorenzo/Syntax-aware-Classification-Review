ee546da55af55e04152c2e4103665e03c1162d7f
sofiapedros <202108146@alu.comillas.edu>
2024-04-19 18:59:57 +0200

    Design experiments to evaluate the scope of negation

---
+++ src/Experiments/main.py
@@ -0,0 +1,117 @@
+from typing import List
+
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.data import download_data, load_trees
+from src.treebank import Tree
+from src.RecursiveModel.recursive import RNTN
+from src.RecurrentModel.models import RNN
+from src.RecursiveModel.utils import set_seed
+from src.Experiments.utils import read_indices, count_correct
+
+set_seed(42)
+
+
+def main() -> None:
+    """
+    Args:
+    -None
+
+    Returns:
+    -None
+    """
+
+    download_data()
+    train_data: List[Tree] = load_trees("train")
+
+    device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    name_recursive: str = "RecNN_55.26"
+    name_RTNT: str = "RTNT_58.19"
+    name_recursive_2: str = "RecNN_63.03_embeddings_propios"
+    name_RNN: str = "RNN_33_embeddings_torch"
+    name_RNN_2: str = "RNN_31.06_embeddings_propios"
+
+    # Load all models models
+    model_simple: RNTN = torch.load(
+        f"models/{name_recursive}.pt", map_location=torch.device("cpu")
+    )
+    model_simple.device = device
+    model_simple.to(device)
+
+    ##
+    model_simple_2: RNTN = torch.load(
+        f"models/{name_recursive_2}.pt", map_location=torch.device("cpu")
+    )
+    model_simple_2.device = device
+    model_simple_2.to(device)
+
+    ##
+    model_RTNT: RNTN = torch.load(
+        f"models/{name_RTNT}.pt", map_location=torch.device("cpu")
+    )
+    model_RTNT.device = device
+    model_RTNT.to(device)
+
+    ##
+    # model_RNN: RNN = torch.load(
+    #     f"models/{name_RNN}.pt", map_location=torch.device("cpu")
+    # )
+    # model_RNN.device = device
+    # model_RNN.to(device)
+
+    # ##
+    # model_RNN_2: RNN = torch.load(
+    #     f"models/{name_RNN_2}.pt", map_location=torch.device("cpu")
+    # )
+    # model_RNN_2.device = device
+    # model_RNN_2.to(device)
+
+    # Get the trees of the negated phrases
+    filepath: str = "./src/Experiments/negation_phrases_indexes.txt"
+    indices: List[int] = read_indices(filepath=filepath)
+    filtered_trees: List[Tree] = [train_data[indice] for indice in indices]
+
+    # Get real labels
+    labels: List[int] = []
+    for indice in indices:
+        label: int = train_data[indice].labels[-1]
+        labels.append(label)
+
+    # Compute outputs
+    outputs_simple: torch.Tensor = model_simple(filtered_trees, root_only=True)
+    real_predictions_simple: torch.Tensor = torch.argmax(outputs_simple, dim=1)
+    correct_predictions_simple: int = count_correct(real_predictions_simple, labels)
+
+    outputs_simple_2: torch.Tensor = model_simple_2(filtered_trees, root_only=True)
+    real_predictions_simple_2: torch.Tensor = torch.argmax(outputs_simple_2, dim=1)
+    correct_predictions_simple_2: int = count_correct(real_predictions_simple_2, labels)
+
+    outputs_RTNT: torch.Tensor = model_RTNT(filtered_trees, root_only=True)
+    real_predictions_RTNT: torch.Tensor = torch.argmax(outputs_RTNT, dim=1)
+    correct_predictions_RTNT: int = count_correct(real_predictions_RTNT, labels)
+
+    # Plot results
+    models: List[str] = [
+        "Simple_torch_embeddings",
+        "RNTN_torch_embeddings",
+        "RNTN_own_embeddings",
+    ]
+    counts: List[int] = [
+        correct_predictions_simple,
+        correct_predictions_simple_2,
+        correct_predictions_RTNT,
+    ]
+
+    plt.bar(models, counts, color="maroon", width=0.4)
+
+    plt.xlabel("Models")
+    plt.ylabel("Correct predictions")
+    plt.title("Correct predictions of negated sentences")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/Experiments/utils.py
@@ -0,0 +1,39 @@
+from typing import List
+import torch
+
+
+def read_indices(filepath: str) -> List[int]:
+    """
+    Get the indices of the phrases that have negationos.
+    Args:
+    - filepath(str): path of the file with the desired indices.
+    Returns:
+    - indices (List[int]): indices of the desired sentences
+    """
+
+    with open(filepath, "r") as file:
+        text: str = file.readline()
+
+    indices_str: List[str] = text.split(",")
+
+    indices = [int(index.strip()) - 1 for index in indices_str]
+
+    return indices
+
+
+def count_correct(predictions: List[torch.Tensor], labels: List[int]) -> int:
+    """
+    Count correct predictions given the labels.
+    Args:
+    - predictions (List[int]): list of model predictions (as tensor)
+    - labels (List[int]): list of correct labels
+    Returns:
+    - correct (int): number of correct predictions
+    """
+    correct: int = 0
+
+    for prediction, label in zip(predictions, labels):
+        if prediction.item() == label:
+            correct += 1
+
+    return correct
