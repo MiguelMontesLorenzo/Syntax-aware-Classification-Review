6cb7c9386155a9a44eb5c5a61b4c217ab70559bb
Natalia Leyenda Lodares <202108204@alu.comillas.edu>
2024-04-21 01:26:27 +0200

    evaluate checkpoints

--- src/RecursiveModel/evaluate_checkpoints.py
+++ src/RecursiveModel/evaluate_checkpoints.py
@@ -1,33 +1,28 @@
 from typing import List, Dict
 import torch
-from torch.utils.tensorboard import SummaryWriter
-from tqdm.auto import tqdm
-from torch.optim.lr_scheduler import StepLR
 import os
-
+ 
 from src.data import download_data, load_trees, generate_dataloaders, load_vocab
 from src.treebank import Tree
 from src.utils import load_pretrained_weights, save_model
 from src.pretrained_embeddings import SkipGramNeg
 from src.RecursiveModel.recursive import RNTN
-from src.RecursiveModel.train_functions import train, val
+from src.RecursiveModel.train_functions import train, test
 from src.RecursiveModel.utils import set_seed
-
+ 
 set_seed(42)
-
-
-def main() -> None:
+ 
+ 
+def main(num: int) -> None:
     """
+    Evaluate the best_model
     Args:
-    -None
-
-    Returns:
-    -None
+    - None
+    Returns
+    - None
     """
-
-    download_data()
-
-    # hyperparameters
+    device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+ 
     hidden_size: int = 300
     lr: float = 0.02
     epochs: int = 30
@@ -39,28 +34,20 @@ def main() -> None:
     our_embeddings = True
     patience: int = 20
     best_val_loss: float = float("inf")
-
-    device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+ 
     # empty nohup file
     open("nohup.out", "w").close()
-
+ 
     # Load training, validation and test data
     train_data: List[Tree] = load_trees("train")
-    # test_data: List[Tree] = load_trees("test")
-    val_data: List[Tree] = load_trees("dev")
-
+ 
     word2index: Dict[str, int]
     word2index, _ = load_vocab(train_data)
-
-    # define name and writer
-    name: str = (
-        f"model_lr_{lr}_hs_{hidden_size}_bs_{batch_size}_e_{epochs}_ss_{step_size}_g{gamma}_simple_{simple_RNN}"
-    )
-    writer: SummaryWriter = SummaryWriter(f"runs/{name}")
-
+ 
     print(device)
-
+ 
+    name: str = f"checkpoint_{num}"
+ 
     # our own embeddings
     if our_embeddings:
         pretrained_folder: str = "pretrained"
@@ -107,57 +94,14 @@ def main() -> None:
             device=device,
         ).to(device)
 
-    # Define loss function and optimizer
-    loss: torch.nn.Module = torch.nn.CrossEntropyLoss()
-    optimizer: torch.optim.Optimizer = torch.optim.Adagrad(model.parameters(), lr=lr)
+ 
+    model.load_state_dict(torch.load(f"models/{name}")["model_state_dict"])
+ 
+    test_data: List[Tree] = load_trees("test")
 
-    # Learning rate scheduler
-    scheduler: StepLR = StepLR(optimizer, step_size=step_size, gamma=gamma)
-
-    for epoch in tqdm(range(epochs)):
-        train(
-            model,
-            batch_size,
-            train_data,
-            device,
-            optimizer,
-            loss,
-            writer,
-            epoch,
-        )
-        val_loss: float = val(model, batch_size, val_data, device, loss, writer, epoch)
-
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            no_improvement = 0
-        else:
-            no_improvement += 1
-            if no_improvement >= patience:
-                print(f"No improvement for {patience} epochs. Early stopping...")
-                break
-
-        # Save checkpoints
-        if (epoch + 1) % 2 == 0:
-            # create folder if it does not exist
-            if not os.path.isdir("models"):
-                os.makedirs("models")
-
-            torch.save(
-                {
-                    "epoch": epoch,
-                    "model_state_dict": model.state_dict(),
-                    "optimizer_state_dict": optimizer.state_dict(),
-                    "loss": loss,
-                },
-                f"models/checkpoint_{epoch+1}",
-            )
-
-        # Update the scheduler
-        scheduler.step()
-
-    # Save model
-    save_model(model, "best_model")
-
-
+    accuracy_value: float = test(model, batch_size, test_data, device)
+    print("Test accuracy:", accuracy_value)
+ 
+ 
 if __name__ == "__main__":
-    main()
+    main(10)
