7ecbe1c9ba89741b7282e834a837524ee8ff82d3
NatLey30 <112613481+NatLey30@users.noreply.github.com>
2024-04-22 19:20:55 +0200

    [MERGE]: Recover recurrent model files

---
+++ src/RecurrentModel/main.py
@@ -0,0 +1,159 @@
+from typing import List, Dict
+import torch
+import os
+from torch.utils.tensorboard import SummaryWriter
+from tqdm.auto import tqdm
+
+# from torch.optim.lr_scheduler import StepLR
+
+from src.data import download_data, load_trees, generate_dataloaders, load_vocab
+from src.utils import save_model, load_pretrained_weights
+from src.treebank import Tree
+from src.pretrained_embeddings import SkipGramNeg
+from src.RecurrentModel.models import RNN
+from src.RecurrentModel.train_functions import train_step, val_step, t_step
+
+
+def main() -> None:
+    """
+    Args:
+    -None
+
+    Returns:
+    -None
+    """
+
+    download_data()
+
+    # Constants
+    NUM_CLASSES: int = 5
+    EMBED_DIM: int = 300
+
+    # Hyperparameters
+    hidden_size: int = 24
+    embedding_dim: int = 300
+    learning_rate: float = 1e-3
+    epochs: int = 50
+    batch_size: int = 128
+    # step_size: int = 20
+    # gamma: float = 0.1
+    our_embeddings: bool = False
+
+    patience: int = 10
+    best_val_loss: float = float("inf")
+
+    # Load training, validation and test data
+    train_data: List[Tree] = load_trees("train")
+
+    word2index: Dict[str, int]
+    word2index, _ = load_vocab(train_data)
+
+    device: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    device = "cpu"
+
+    download_data()
+
+    # empty nohup file
+    open("nohup.out", "w").close()
+
+    # define writer
+    name: str = "fri_1"
+    writer: SummaryWriter = SummaryWriter(f"runs/{name}")
+
+    train_loader, val_loader, test_loader, vocab_to_int, _ = (
+            generate_dataloaders(batch_size=batch_size)
+        )
+
+    # our own embeddings
+    if our_embeddings:
+        pretrained_folder: str = "pretrained"
+        pretrained_weights_filename: str = "skipgram_dep_model_updated.pth"
+        big_vocab_to_int: str = "vocab_to_int.json"
+        vocab_size: int = len(vocab_to_int)
+
+        pretrained_model: SkipGramNeg = SkipGramNeg(vocab_size, embed_dim=EMBED_DIM)
+        pretrained_weights_path: str = os.path.join(
+            pretrained_folder, pretrained_weights_filename
+        )
+        pretrained_dict_path: str = os.path.join(pretrained_folder, big_vocab_to_int)
+
+        weights: torch.Tensor = load_pretrained_weights(
+            pretrained_weights_path, pretrained_dict_path, vocab_to_int
+        )
+
+        pretrained_model.load_pretrained_embeddings(weights)
+
+        # Freeze pretrained model parameters
+        for param in pretrained_model.parameters():
+            param.requires_grad = False
+
+        # Define model
+        model: RNN = RNN(
+            word2index=word2index,
+            embedding_dim=embedding_dim,
+            hidden_dim=hidden_size,
+            num_classes=NUM_CLASSES,
+            num_layers=1,
+            bidirectional=True,
+            device=device,
+            pretrained_model=pretrained_model,
+        ).to(device)
+
+    else:
+        # Define model
+        model: RNN = RNN(
+            word2index=vocab_to_int,
+            embedding_dim=embedding_dim,
+            hidden_dim=hidden_size,
+            num_classes=NUM_CLASSES,
+            num_layers=1,
+            bidirectional=True,
+            device=device,
+        ).to(device)
+
+    # Define loss function and optimizer
+    loss: torch.nn.Module = torch.nn.CrossEntropyLoss()
+    optimizer: torch.optim.Optimizer = torch.optim.AdamW(
+        model.parameters(), lr=learning_rate
+    )
+
+    # Learning rate scheduler
+    # scheduler: StepLR = StepLR(optimizer, step_size=step_size, gamma=gamma)
+
+    print(f"Training Recurrent model on {device}...")
+    print("Len vocab", len(vocab_to_int))
+
+    # Train loop
+    for epoch in tqdm(range(epochs)):
+        # Call train step
+        total_train_lost: float = train_step(
+            model, train_loader, loss, optimizer, writer, epoch, device
+        )
+
+        # Call val step
+        val_step(model, val_loader, loss, writer, epoch, device)
+
+        # Check for Early Stopping
+        if total_train_lost < best_val_loss:
+            best_val_loss = total_train_lost
+            epochs_no_improve = 0
+        else:
+            epochs_no_improve += 1
+            if epochs_no_improve >= patience:
+                print(
+                    f"Early stopping triggered at epoch {epoch}. No improvement in test loss for {patience} consecutive epochs."
+                )
+                break
+
+        # scheduler.step()
+
+    # Save model
+    save_model(model, "best_model")
+
+    accuracy_value: float = t_step(model, test_loader, device)
+    print("Test accuracy:", accuracy_value)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/RecurrentModel/train_functions.py
@@ -0,0 +1,146 @@
+from typing import List
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from src.utils import accuracy
+
+
+def train_step(
+    model: torch.nn.Module,
+    train_data: DataLoader,
+    loss: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    writer: SummaryWriter,
+    epoch: int,
+    device: torch.device,
+) -> float:
+    """
+    This function computes the training step.
+
+    Args:
+    - model (torch.nn.Module): pytorch model.
+    - train_data (DataLoader): train dataloader.
+    - loss (torch.nn.Module): loss function.
+    - optimizer (torch.optim.Optimizer): optimizer object.
+    - writer (SummaryWriter): tensorboard writer.
+    - epoch (int): epoch number.
+    - device (torch.device): device of model.
+
+    Return:
+    - final_loss (float): average loss for the early stopping.
+    """
+
+    # Define metric lists
+    losses: List[float] = []
+    accuracies: List[float] = []
+
+    model.train()
+
+    for sentences, labels, text_len in train_data:
+        sentences: torch.Tensor = sentences.to(device)
+        labels: torch.Tensor = labels.to(device)
+
+        outputs: torch.Tensor = model(sentences, text_len)
+
+        loss_value: torch.nn.Module = loss(outputs, labels.long())
+
+        accuracy_measure: torch.Tensor = accuracy(outputs, labels)
+
+        optimizer.zero_grad()
+        loss_value.backward()
+        optimizer.step()
+
+        losses.append(loss_value.item())
+        accuracies.append(accuracy_measure.item())
+
+    final_loss: float = float(np.mean(losses))
+
+    # write on tensorboard
+    writer.add_scalar("train/loss", final_loss, epoch)
+    writer.add_scalar("train/accuracy", np.mean(accuracies), epoch)
+
+    return final_loss
+
+
+def val_step(
+    model: torch.nn.Module,
+    val_data: DataLoader,
+    loss: torch.nn.Module,
+    writer: SummaryWriter,
+    epoch: int,
+    device: torch.device,
+) -> None:
+    """
+    This function computes the validation step.
+
+    Args:
+    - model (torch.nn.Module): pytorch model.
+    - val_data (DataLoader): val dataloader.
+    - loss (torch.nn.Module): loss function.
+    - writer (SummaryWriter): tensorboard writer.
+    - epoch (int): epoch number.
+    - device (torch.device): device of model.
+
+    Return:
+    - None
+    """
+
+    model.eval()
+
+    with torch.no_grad():
+        losses: List[float] = []
+        accuracies: List[float] = []
+
+        for sentences, labels, text_len in val_data:
+            sentences: torch.Tensor = sentences.to(device)
+            labels: torch.Tensor = labels.to(device)
+
+            outputs: torch.Tensor = model(sentences, text_len)
+
+            loss_value: torch.nn.Module = loss(outputs, labels.long())
+
+            accuracy_measure: torch.Tensor = accuracy(outputs, labels)
+
+            losses.append(loss_value.item())
+            accuracies.append(accuracy_measure.item())
+
+        writer.add_scalar("val/loss", np.mean(losses), epoch)
+        writer.add_scalar("val/accuracy", np.mean(accuracies), epoch)
+
+
+def t_step(
+    model: torch.nn.Module,
+    test_data: DataLoader,
+    device: torch.device,
+) -> float:
+    """
+    This function computes the test step.
+
+    Args:
+    - model (torch.nn.Module): pytorch model.
+    - test_data (DataLoader): test dataloader.
+    - device (torch.device): device of model.
+
+    Returns:
+    - final_accuracy (float): average accuracy.
+    """
+
+    model.eval()
+    accuracies: List[float] = []
+
+    with torch.no_grad():
+
+        for sentences, labels, text_len in test_data:
+            sentences: torch.Tensor = sentences.to(device)
+            labels: torch.Tensor = labels.to(device)
+
+            outputs: torch.Tensor = model(sentences, text_len)
+
+            accuracy_measure: torch.Tensor = accuracy(outputs, labels)
+
+            accuracies.append(accuracy_measure.item())
+
+        final_accuracy: float = float(np.mean(accuracies))
+    return final_accuracy
