3eec7178f82d5e87ad401431fe30dc6bd1c7e6c0
Miguel Montes Lorenzo <112613446+MiguelMontesLorenzo@users.noreply.github.com>
2024-04-23 09:11:12 +0200

    evaluate weighter added + type hinting

--- src/VecAvgModel/evaluate.py
+++ src/VecAvgModel/evaluate.py
@@ -1,23 +1,27 @@
-# deep learning libraries
 import torch
-
-# from torch.utils.data import DataLoader
-from torch.jit import RecursiveScriptModule
-
-from typing import Final
-from torch.utils.data import DataLoader
+import os
 
 from src.data import generate_dataloaders
+from src.utils import load_pretrained_weights
 from src.treebank import Tree
-from src.VecAvgModel.train_functions import test_step
-from src.VecAvgModel.models import Weighter, UniformWeighter
+from src.pretrained_embeddings import SkipGramNeg
+from src.models import VecAvg, Weighter, UniformWeighter, NaiveBayesWeighter
 
-# static variables
-DATA_PATH: Final[str] = "data"
-NUM_CLASSES: Final[int] = 10
+from torch.utils.data import DataLoader
+
+# from typing import Final
+from src.train_functions import test_step
+
+# params
+EMBED_DIM: int = 300
+NUM_CLASSES: int = 5
+HIDDEN_SIZES: tuple[int, int, int] = (264, 264, 64)
+use_naive_bayes: bool = True
 
 # set device
-device: str = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device: torch.device = (
+    torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+)
 
 
 def main() -> None:
@@ -30,32 +34,71 @@ def main() -> None:
     # load data
     test_loader: DataLoader
     vocab_to_int: dict[str, int]
-    test_data: list[Tree]
-    (
-        _,
-        _,
-        test_loader,
-        vocab_to_int,
-        _,
-        _,
-        _,
-        test_data,
-    ) = generate_dataloaders(batch_size=batch_size)
+    train_data: list[Tree]
+    val_data: list[Tree]
+    (_, _, test_loader, vocab_to_int, _, train_data, val_data, _) = (
+        generate_dataloaders(batch_size=batch_size)
+    )
+
+    pretrained_folder: str = "pretrained"
+    pretrained_weights_filename: str = "skipgram_dep_model_updated.pth"
+    big_vocab_to_int: str = "vocab_to_int.json"
+    vocab_size: int = len(vocab_to_int)
+
+    pretrained_model: SkipGramNeg = SkipGramNeg(vocab_size, embed_dim=EMBED_DIM)
+    pretrained_weights_path: str = os.path.join(
+        pretrained_folder, pretrained_weights_filename
+    )
+    pretrained_dict_path: str = os.path.join(pretrained_folder, big_vocab_to_int)
+
+    weights: torch.Tensor = load_pretrained_weights(
+        pretrained_weights_path, pretrained_dict_path, vocab_to_int
+    )
+
+    pretrained_model.load_pretrained_embeddings(weights)
+
+    # Freeze pretrained model parameters
+    for param in pretrained_model.parameters():
+        param.requires_grad = False
 
     # define name and writer
-    name: str = "model_20240422_183617"
+    name: str = "model_name"
 
+    naive_bayes_params: tuple[list[list[int]], list[int]]
+    sentences: list[list[int]] = []
+    labels: list[int] = []
+    weighter: Weighter
+    for tree in train_data:
+        sentences.append([vocab_to_int[word] for word in tree.get_words()])
+        labels.append(tree.labels[-1])
+    for tree in val_data:
+        sentences.append([vocab_to_int[word] for word in tree.get_words()])
+        labels.append(tree.labels[-1])
+
+    unique_labels: torch.Tensor = torch.unique(torch.Tensor(labels))
+
+    if use_naive_bayes:
+        naive_bayes_params = (sentences, labels)
+        weighter = NaiveBayesWeighter(
+            vocab_size=vocab_size, output_size=unique_labels.shape[0]
+        )
+        weighter.fit(*naive_bayes_params)
+    else:
+        weighter = UniformWeighter(
+            vocab_size=vocab_size, output_size=unique_labels.shape[0]
+        )
     # Load the model
-    model: RecursiveScriptModule = torch.jit.load(f"models/{name}/{name}.pt").to(device)
-
-    # # Load the constants
-    # with open(f"models/{name}/constants.pkl", "rb") as f:
-    #     constants = pickle.load(f)
-    # model.weighter.set_constants(constants)
+    model: torch.nn.Module = VecAvg(
+        input_size=EMBED_DIM,
+        output_size=NUM_CLASSES,
+        hidden_sizes=HIDDEN_SIZES,
+        pretrained_model=pretrained_model,
+    ).to(device)
+    model.load_state_dict(torch.load(f"models/{name}/{name}.pth"))
 
     # call test step and evaluate accuracy
-    weighter: Weighter = UniformWeighter
-    accuracy: float = test_step(model, test_data, device, weighter)
+    weighter: Weighter = UniformWeighter()
+    accuracy: float = test_step(model, test_loader, device, weighter, NUM_CLASSES)
     print(f"accuracy: {accuracy}")
 
     return None
