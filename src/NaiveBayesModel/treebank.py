a24884b762ac8e6807d70ec3d7b3ac20f66bb848
Miguel Montes Lorenzo <112613446+MiguelMontesLorenzo@users.noreply.github.com>
2024-04-23 09:04:53 +0200

    treebank.py type hinting

--- src/NaiveBayesModel/treebank.py
+++ src/NaiveBayesModel/treebank.py
@@ -1,19 +1,21 @@
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 
 class Node:
-    def __init__(self, label: int, word: str, parent=None) -> None:
+    def __init__(self, label: int, word: Optional[str] = None, parent=None) -> None:
         """
         Initialize Node class.
+
         Args:
         - label (int): sentiment label
         - word (str): word in the sentence
         - parent (Optional[Node]): node parent
+
         Returns:
         - None
         """
         self.label: int = label
-        self.word: str = word
+        self.word: Optional[str] = word
         self.parent: Optional[Node] = parent
         self.left: Optional[Node] = None
         self.right: Optional[Node] = None
@@ -22,8 +24,10 @@ class Node:
     def __str__(self) -> str:
         """
         Print the string ofof the node and it's children.
+
         Args:
         - None
+
         Returns:
         - None
         """
@@ -40,11 +44,15 @@ class Tree:
     ) -> None:
         """
         Initialize Tree class.
+
         Args:
         - treeString (str): string extracted from the dataset, is equivalent
             to a tree.
         - openChar (str): string that delimits the start of a constituyent.
         - closeChar (str): string that delimits the start of a constituyent.
+
+        Returns:
+        - None
         """
         tokens: List = []
         self.open: str = openChar
@@ -54,17 +62,19 @@ class Tree:
             tokens += list(toks)
         self.root: Node = self.parse(tokens)
 
-        self.labels: List[int] = self.get_labels(self.root)
+        self.labels: List[Optional[int]] = self.get_labels(self.root)
         self.num_words: int = len(self.labels)
 
     def parse(self, tokens: List[str], parent: Optional[Node] = None) -> Node:
         """
         Parse a list of tokens (equivalent) to a sentence (or a subsentence)
         to convert the complete sentence to a tree structure.
+
         Args:
         - tokens (List[str]): list of tokens to parse.
-        - parent (Optional[Node]): Node to start parsing,  if None, is the complete
-          sentence.
+        - parent (Optional[Node]):
+            Node to start parsing,  if None, is the complete sentence.
+
         Returns:
         - node (Node): Node corresponding to the parent of the tree.
         """
@@ -88,11 +98,11 @@ class Tree:
             split += 1
 
         # New node
-        node: Node = Node(int(tokens[1]), parent)
+        node: Node = Node(int(tokens[1]), parent=parent)
 
         # Leaf Node
         if countOpen == 0:
-            node.word = "".join(tokens[2:-1]).lower()
+            node.word = clean_sentence("".join(tokens[2:-1]).lower())
             node.isLeaf = True
             return node
 
@@ -102,19 +112,21 @@ class Tree:
 
         return node
 
-    def get_words(self) -> List[str]:
+    def get_words(self) -> List[Optional[str]]:
         """
         Get the words of the sentence to parse
+
         Args:
         - None
+
         Returns:
         - words (List[str]): list of words corresponding to the sentence
         """
-        leaves: List[Node] = self.get_leaves(self.root)
-        words: List[str] = [node.word for node in leaves]
+        leaves: List[Optional[Node]] = self.get_leaves(self.root)
+        words: List[Optional[str]] = [node.word for node in leaves if node]
         return words
 
-    def get_labels(self, node: Node) -> List[int]:
+    def get_labels(self, node: Optional[Node]) -> List[Optional[int]]:
         """
         Gets the terminal labels of the sentence starting with the node.
 
@@ -128,7 +140,7 @@ class Tree:
             return []
         return self.get_labels(node.left) + self.get_labels(node.right) + [node.label]
 
-    def get_leaves(self, node: Node) -> List[Node]:
+    def get_leaves(self, node: Optional[Node]) -> List[Optional[Node]]:
         """
         Gets the terminal nodes of the sentence starting with the node.
 
@@ -143,4 +155,22 @@ class Tree:
         if node.isLeaf:
             return [node]
         else:
-            return self.get_leaves(node.left) + self.get_leaves(node.right)
+            return self.get_leaves(node.left) + self.get_leaves(node.right) + []
+
+
+def clean_sentence(sentence: str) -> str:
+    """
+    Replaces incorrectly formatted characters.
+
+    Args:
+    - sentence (str): sentence to be cleaned.
+
+    Returns:
+    - sentence (str): cleaned sentence.
+    """
+
+    substitutions: Dict[str, str] = {"``": "", "''": ""}
+
+    for key, value in substitutions.items():
+        sentence = sentence.replace(key, value)
+    return sentence.lower()
